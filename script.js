49529c9e116eccde43deb31a067d25d8fbb29149
maskudo <manujkarki101@gmail.com>
2021-06-26 17:57:53 +0545

    added eventlistener to display controller

--- script.js
+++ script.js
@@ -1,5 +1,6 @@
 const Player = (sign)=>{
     const getSign = ()=> {return sign}
+    return {getSign}
 }
 const gameBoard = (()=>{
     let board = [
@@ -7,13 +8,14 @@ const gameBoard = (()=>{
     '','','',
     '','','',
 ]
+
     function getBoard(){
         return board
     }
     function setValue(index,sign){
-        if(board[index]!=''){
+        if(board[index]==''){
             board[index]=sign
-
+            displayController.updateDisplay(board)
         } 
     }
     return {getBoard,setValue}
@@ -22,7 +24,11 @@ const gameBoard = (()=>{
 const displayController = (()=>{
     let gridItems = []
     const container = document.querySelector(".container")
+    const turnElement = document.querySelector("#turn")
+    const player1 = Player('X')
+    const player2 = Player('O')
 
+    let currentTurn = player1.getSign()
     function init(){
         for(let i=0;i<9;i++){
             gridItems[i]=document.createElement('div')
@@ -30,15 +36,33 @@ const displayController = (()=>{
             gridItems[i].dataset.number = `${i}`
             container.append(gridItems[i])
         }
-        console.log(gridItems)
+        turnElement.textContent = `Turn: ${currentTurn}`
+        gridItems.forEach(item=>{
+            item.addEventListener('click',()=>{
+                let index = item.dataset.number
+                let sign = currentTurn
+                gameBoard.setValue(index,sign)
+            })
+        })
+
     }
     function updateDisplay(board){
         for(let i=0;i<9;i++){
             gridItems[i].textContent = board[i]
         }
+        if(currentTurn==player1.getSign()){
+            currentTurn = player2.getSign()
+        }
+        else{
+            currentTurn = player1.getSign()
+        }
+        turnElement.textContent = `Turn: ${currentTurn}`
     }
+
     return {init,updateDisplay}
 })()
 
+const gameController = (()=>{
+
+})()
 displayController.init()
-displayController.updateDisplay(gameBoard.getBoard())
